e3fbb1c55a53312b2ef454ce9fe1044400c4b967
charlyn23 <charlyn.buchanan@gmail.com>
2015-05-17 14:39:46 -0400

    numbers added to view

---
+++ app/src/main/java/charlyn23/c4q/nyc/calc/AutoResize.java
@@ -0,0 +1,33 @@
+//package charlyn23.c4q.nyc.calc;
+//
+//import android.widget.TextView;
+//import android.content.Context;
+//import android.text.Layout.Alignment;
+//import android.text.StaticLayout;
+//import android.text.TextPaint;
+//import android.util.AttributeSet;
+//import android.util.TypedValue;
+//import android.widget.TextView;
+///**
+// * Created by charlynbuchanan on 5/17/15.
+// */
+//public class AutoResize {
+//    public static final float MIN_TEXT_SIZE = 20;
+//
+//    public interface OnTextResizeListener {
+//        public void onTextResize(TextView textView, float oldSize, float newSize);
+//    }
+//    // Registered resize listener
+//    private OnTextResizeListener mTextResizeListener;
+//
+//    // Flag for text and/or size changes to force a resize
+//    private boolean mNeedsResize = false;
+//
+//    // Text size that is set from code. This acts as a starting point for resizing
+//    private float mTextSize;
+//
+//    public AutoResize(Context context) {
+//        this(context, null);
+//    }
+//
+//}
--- app/src/main/java/charlyn23/c4q/nyc/calc/Main.java
+++ app/src/main/java/charlyn23/c4q/nyc/calc/Main.java
@@ -5,99 +5,23 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 
 public class Main extends ActionBarActivity {
-    String express="";
-    int expLength = 0;
-    String answer ="";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button[] b = new Button[35];
+        b[0]=(Button)findViewById(R.id.help); b[1]=(Button)findViewById(R.id.deg);
     }
-    public void bottonclick(View v){
-        if(v.getId()==R.id.help){
-            //TODO add activity and set explicit intent
-        }
-        if(v.getId()==R.id.deg){
-            express+="*(180/PI)";
-            expLength=8;
-            
-        } if(v.getId()==R.id.perm){
 
-            
-        } if(v.getId()==R.id.openParen){
-            express+="(";
-            expLength=1;
-        } if(v.getId()==R.id.closeParen){
-            express+=")";
-            expLength=1;
-        } if(v.getId()==R.id.percent){
-            express+="%";
-            expLength=1;
-        } if(v.getId()==R.id.clear){
-            express="";
-        } if(v.getId()==R.id.tan){
-            express+="TAN(";
-            expLength=4;
-        } if(v.getId()==R.id.point){
-            express+=".";
-            expLength=1;
-        } if(v.getId()==R.id.sin){
-
-        } if(v.getId()==R.id.ln){
-            
-        } if(v.getId()==R.id.cos){
-            
-        } if(v.getId()==R.id.one){
-            
-        } if(v.getId()==R.id.two){
-            
-        } if(v.getId()==R.id.three){
-            
-        } if(v.getId()==R.id.four){
-            
-        } if(v.getId()==R.id.five){
-            
-        } if(v.getId()==R.id.six){
-            
-        } if(v.getId()==R.id.seven){
-            
-        } if(v.getId()==R.id.eight){
-            
-        } if(v.getId()==R.id.nine){
-            
-        } if(v.getId()==R.id.zero){
-            
-        } if(v.getId()==R.id.div){
-            
-        } if(v.getId()==R.id.multiply){
-            
-        } if(v.getId()==R.id.minus){
-            
-        } if(v.getId()==R.id.plus){
-            
-        } if(v.getId()==R.id.pi){
-            
-        } if(v.getId()==R.id.e){
-            
-        } if(v.getId()==R.id.sqrt){
-            
-        } if(v.getId()==R.id.ans){
-            
-        } if(v.getId()==R.id.exp){
-            
-        } if(v.getId()==R.id.equals){
-            
-        } if(v.getId()==R.id.power){
-            
-        } if(v.getId()==R.id.percent){
-
-        } if(v.getId()==R.id.inv){
-            
-        }
-    }
 
 
     @Override
